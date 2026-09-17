ae2a5d424ac8ec8fc81ba4b606cc864e1a50fe7e
Ngahoang2005 <158808113+Ngahoang2005@users.noreply.github.com>
2026-09-17 19:13:31 +0700

    Update run_funsd_cord.py

--- examples/run_funsd_cord.py
+++ examples/run_funsd_cord.py
@@ -390,7 +390,7 @@ def main():
 
                 optimizer_grouped_parameters = [
                     {"params": backbone_params, "lr": self.args.learning_rate},
-                    {"params": new_params, "lr": 5e-4}
+                    {"params": new_params, "lr": 2e-4}
                 ]
 
                 self.optimizer = torch.optim.AdamW(
